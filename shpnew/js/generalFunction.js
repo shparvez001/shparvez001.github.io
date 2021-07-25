dbabaf94bfd7a0cc3ffb47d507f4587790aba536
Shahadat Hussain Parvez <shparvez001@gmail.com>
2021-07-25 20:37:12 +0600

    Swipe option for image gallery added.

--- shpnew/js/generalFunction.js
+++ shpnew/js/generalFunction.js
@@ -87,6 +87,65 @@ document.onkeydown = function(evt) {
     }
 };
 
+//Swipe detection
+var container = document.getElementById("galleryModal");
+
+  container.addEventListener("touchstart", startTouch, false);
+  container.addEventListener("touchmove", moveTouch, false);
+
+  // Swipe Up / Down / Left / Right
+  var initialX = null;
+  var initialY = null;
+
+  function startTouch(e) {
+    initialX = e.touches[0].clientX;
+    initialY = e.touches[0].clientY;
+  };
+
+  function moveTouch(e) {
+    if (initialX === null) {
+      return;
+    }
+
+    if (initialY === null) {
+      return;
+    }
+
+    var currentX = e.touches[0].clientX;
+    var currentY = e.touches[0].clientY;
+
+    var diffX = initialX - currentX;
+    var diffY = initialY - currentY;
+
+    if (Math.abs(diffX) > Math.abs(diffY)) {
+      // sliding horizontally
+      if (diffX > 0) {
+        // swiped left
+        console.log("swiped left");
+		plusSlides(1);
+      } else {
+        // swiped right
+        console.log("swiped right");
+		plusSlides(-1);
+      }  
+    } else {
+      // sliding vertically
+      if (diffY > 0) {
+        // swiped up
+        console.log("swiped up");
+      } else {
+        // swiped down
+        console.log("swiped down");
+      }  
+    }
+
+    initialX = null;
+    initialY = null;
+
+    e.preventDefault();
+  };
+
+
 }
 
 
@@ -139,3 +198,4 @@ document.onkeydown = function(evt) {
 	  console.log(slides[slideIndex-1].title);
 	}
 //Image Gallery function ends
+
